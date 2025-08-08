756d142eebae8c55938c4dd51fcff33c8bf1421a
dizhi669 <woai91aaa@gmail.com>
2025-08-08 11:55:32 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[];
-       urls.push(".eaiwxnrp.top"); 
-       urls.push(".urinbim.com");
-       urls.push(".iqjjahp.top");   
+       urls.push(".wpqkehf.biz"); 
+       urls.push(".eaiwxnrp.top");
+       urls.push(".urinbim.com");   
       
       		
-var JumpPage="https://recvcyuc.top";
+var JumpPage="https://hjvideo1.com";
 
 var newestUrls = [];
 
@@ -164,4 +164,5 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
