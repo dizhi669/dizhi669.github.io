e1edbc3bce6054e4b10974f06124c4012cb8a590
dizhi669 <woai91aaa@gmail.com>
2025-08-15 15:40:49 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -42,7 +42,7 @@ var urls=[];
        urls.push(".eaiwxnrp.top");   
       
       		
-var JumpPage="https://hjvideo1.com";
+var JumpPage="https://hjvideo2.com";
 
 var newestUrls = [];
 
@@ -164,6 +164,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
