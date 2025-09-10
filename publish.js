d2f06bd3a193517c43fd2a561ae83c50953bebee
dizhi669 <woai91aaa@gmail.com>
2025-09-10 13:37:10 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,9 +37,9 @@ var emails = [
 ];
 
 var urls=[];
-       urls.push(".xevwzum.com"); 
-       urls.push(".jeelkge.com");
-       urls.push(".wnhomyo.cc");   
+       urls.push(".cqvjojn.cc"); 
+       urls.push(".xevwzum.com");
+       urls.push(".jeelkge.com");   
       
       		
 var JumpPage="https://hjvideo5.com";
@@ -164,6 +164,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
