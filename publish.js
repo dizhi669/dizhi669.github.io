85f2dacace94155abd1430ff942a9d2716aa1e40
dizhi669 <woai91aaa@gmail.com>
2025-11-15 09:48:45 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[];
-       urls.push(".hdisavf.xyz"); 
+       urls.push(".snrjpaat.com"); 
        urls.push(".mzlmbty.cc");
        urls.push(".vjsnkqsy.xyz");   
       
@@ -164,6 +164,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
