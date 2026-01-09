922e846a33bbc70b4fb423a08eab76b6c8e5ac93
dizhi669 <woai91aaa@gmail.com>
2026-01-09 11:21:58 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[];
-       urls.push(".wvrkvad.cc"); 
-       urls.push(".nkqhacut.xyz");
-       urls.push(".touitsj.cc");   
+       urls.push(".incvcds.com"); 
+       urls.push(".wvrkvad.cc");
+       urls.push(".nkqhacut.xyz");   
       
       		
-var JumpPage="https://hjvideo22.com";
+var JumpPage="https://hjvideo23.com";
 
 var newestUrls = [];
 
@@ -164,6 +164,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
